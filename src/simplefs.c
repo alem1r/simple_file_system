d67e88edb3b74f65d1c82f0a8217cd5d291299d3
alem1r <alessandro.miragliotta.am@gmail.com>
2021-08-29 15:44:47 +0200

    init and format,need testing

--- src/simplefs.c
+++ src/simplefs.c
@@ -116,7 +116,58 @@ static int SimpleFS_removeFileBlock(DirectoryHandle* d, FileBlock* b) {
 
 
 
+DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk) {
 
+    fs->disk = disk;
+
+    FirstDirectoryBlock* first_directory_block = calloc(1, sizeof(FirstDirectoryBlock));
+    int ret = DiskDriver_readBlock(disk, first_directory_block, 0);
+    if (ret == -1) {
+        if (DEBUG) printf("[SFS - init] Formatting disk.\n");
+        SimpleFS_format(fs);
+        int ret = DiskDriver_readBlock(disk, first_directory_block, 0);
+        if (ret == -1)
+            return NULL;
+    }
+
+    DirectoryHandle* dh = malloc(sizeof(DirectoryHandle));
+    dh->sfs = fs;
+    dh->dcb = first_directory_block;
+    dh->directory = NULL;
+    dh->current_block = &first_directory_block->header;
+    dh->pos_in_dir = 0;
+    dh->pos_in_block = 0;
+
+    return dh;
+}
+
+void SimpleFS_format(SimpleFS* fs) {
+    
+    fs->disk->header->free_blocks = fs->disk->header->num_blocks;
+    fs->disk->header->first_free_block = 0;
+    bzero(fs->disk->bitmap_data, fs->disk->header->bitmap_entries);
+    
+    FirstDirectoryBlock first_directory_block = {0};
+
+    first_directory_block.header.previous_block = -1;
+    first_directory_block.header.next_block = -1;
+    first_directory_block.header.block_in_file = 0;
+    first_directory_block.header.block_in_disk = 0;
+
+    first_directory_block.fcb.directory_block = -1;
+    first_directory_block.fcb.size_in_bytes = BLOCK_SIZE; 
+    first_directory_block.fcb.size_in_blocks = 1;
+    first_directory_block.fcb.is_dir = 1;
+    strncpy(first_directory_block.fcb.name, "/", 128);
+
+    first_directory_block.num_entries = 0;
+    
+    int ret = DiskDriver_writeBlock(fs->disk, &first_directory_block, 0);
+    if (ret == -1) {
+        if (DEBUG) printf("[SFS - init] Unable to format disk.\n");
+        return;
+    }
+}
 
 int SimpleFS_createFile(DirectoryHandle* d, const char* filename) {
 
